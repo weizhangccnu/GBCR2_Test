1b0c3011a631688fc614618f4b5193e6c157c289
weizhangccnu <296448435@qq.com>
2020-03-02 17:01:06 -0600

    I2C write and read correctly

--- GBCR2_Test_Control.py
+++ GBCR2_Test_Control.py
@@ -9,9 +9,15 @@ This python script is used to control the GBCR2 I2C slave.
 '''
 #=======================================================================================#
 def main():
+    slave_addr = 0x23
     iss = UsbIss()
-    iss.open("COM8")
+    iss.open("COM3")
     iss.setup_i2c()
+
+    iss.i2c.write(slave_addr, 0, [0, 1, 2])
+
+    data = iss.i2c.read(slave_addr, 0, 3)
+    print(data)
     print("Ok!!!")
 #=======================================================================================#
 if __name__ == '__main__':
