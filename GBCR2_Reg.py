7e615f2ab232c5e7e397dcf2e671941861e24193
weizhangccnu <296448435@qq.com>
2020-03-13 17:18:36 -0500

    dllEnable shifter bit

--- GBCR2_Reg.py
+++ GBCR2_Reg.py
@@ -93,14 +93,14 @@ class GBCR2_Reg(object):
         'Tx1_DL_SR'             :   0x5,
         'Tx1_Dis_DL_Emp'        :   0,
         'Tx1_DL_ATT'            :   0x0,
-        'Tx1_Dis_DL_LPF_BIAS'   :   1,
-        'Tx1_Dis_DL_BIAS'       :   0,
+        'Tx1_Dis_DL_LPF_BIAS'   :   0,
+        'Tx1_Dis_DL_BIAS'       :   1,
 
         'Tx2_DL_SR'             :   0x5,
         'Tx2_Dis_DL_Emp'        :   0,
         'Tx2_DL_ATT'            :   0x0,
-        'Tx2_Dis_DL_LPF_BIAS'   :   1,
-        'Tx2_Dis_DL_BIAS'       :   0,
+        'Tx2_Dis_DL_LPF_BIAS'   :   0,
+        'Tx2_Dis_DL_BIAS'       :   1,
         }
     ## @var register map local to the class
     _regMap = {}
@@ -363,36 +363,36 @@ class GBCR2_Reg(object):
     ## get I2C register value
     def get_config_vector(self):
         reg_value = []
-        reg_value += [hex(self._regMap['CH1_Dis_EQ_LF'] << 5 | self._regMap['CH1_EQ_ATT'] << 3 | self._regMap['CH1_CML_AmplSel'])]
-        reg_value += [hex(self._regMap['CH1_CTLE_HFSR'] << 4 | self._regMap['CH1_CTLE_MFSR'])]
-        reg_value += [hex(self._regMap['CH1_Disable'] << 2 | self._regMap['CH1_Dis_DFF'] << 1 | self._regMap['CH1_Dis_LPF'])]
-        reg_value += [hex(self._regMap['CH2_Dis_EQ_LF'] << 5 | self._regMap['CH2_EQ_ATT'] << 3 | self._regMap['CH2_CML_AmplSel'])]
-        reg_value += [hex(self._regMap['CH2_CTLE_HFSR'] << 4 | self._regMap['CH2_CTLE_MFSR'])]
-        reg_value += [hex(self._regMap['CH2_Disable'] << 2 | self._regMap['CH2_Dis_DFF'] << 1 | self._regMap['CH2_Dis_LPF'])]
-        reg_value += [hex(self._regMap['CH3_Dis_EQ_LF'] << 5 | self._regMap['CH3_EQ_ATT'] << 3 | self._regMap['CH3_CML_AmplSel'])]
-        reg_value += [hex(self._regMap['CH3_CTLE_HFSR'] << 4 | self._regMap['CH3_CTLE_MFSR'])]
-        reg_value += [hex(self._regMap['CH3_Disable'] << 2 | self._regMap['CH3_Dis_DFF'] << 1 | self._regMap['CH3_Dis_LPF'])]
-        reg_value += [hex(self._regMap['CH4_Dis_EQ_LF'] << 5 | self._regMap['CH4_EQ_ATT'] << 3 | self._regMap['CH4_CML_AmplSel'])]
-        reg_value += [hex(self._regMap['CH4_CTLE_HFSR'] << 4 | self._regMap['CH4_CTLE_MFSR'])]
-        reg_value += [hex(self._regMap['CH4_Disable'] << 2 | self._regMap['CH4_Dis_DFF'] << 1 | self._regMap['CH4_Dis_LPF'])]
-        reg_value += [hex(self._regMap['CH5_Dis_EQ_LF'] << 5 | self._regMap['CH5_EQ_ATT'] << 3 | self._regMap['CH5_CML_AmplSel'])]
-        reg_value += [hex(self._regMap['CH5_CTLE_HFSR'] << 4 | self._regMap['CH5_CTLE_MFSR'])]
-        reg_value += [hex(self._regMap['CH5_Disable'] << 2 | self._regMap['CH5_Dis_DFF'] << 1 | self._regMap['CH5_Dis_LPF'])]
-        reg_value += [hex(self._regMap['CH6_Dis_EQ_LF'] << 5 | self._regMap['CH6_EQ_ATT'] << 3 | self._regMap['CH6_CML_AmplSel'])]
-        reg_value += [hex(self._regMap['CH6_CTLE_HFSR'] << 4 | self._regMap['CH6_CTLE_MFSR'])]
-        reg_value += [hex(self._regMap['CH6_Disable'] << 2 | self._regMap['CH6_Dis_DFF'] << 1 | self._regMap['CH6_Dis_LPF'])]
-        reg_value += [hex(self._regMap['CH7_Dis_EQ_LF'] << 5 | self._regMap['CH7_EQ_ATT'] << 3 | self._regMap['CH7_CML_AmplSel'])]
-        reg_value += [hex(self._regMap['CH7_CTLE_HFSR'] << 4 | self._regMap['CH7_CTLE_MFSR'])]
-        reg_value += [hex(self._regMap['CH7_Disable'] << 2 | self._regMap['CH7_Dis_DFF'] << 1 | self._regMap['CH7_Dis_LPF'])]
-        reg_value += [hex(self._regMap['dllEnable'] << 1 | self._regMap['dllCapReset'])]
-        reg_value += [hex(self._regMap['dllForceDown'] << 1 | self._regMap['dllChargePumpCurrent'])]
-        reg_value += [hex(self._regMap['dllClockDelay_CH7'] << 4 | self._regMap['dllClockDelay_CH6'])]
-        reg_value += [hex(self._regMap['dllClockDelay_CH5'] << 4 | self._regMap['dllClockDelay_CH4'])]
-        reg_value += [hex(self._regMap['dllClockDelay_CH3'] << 4 | self._regMap['dllClockDelay_CH2'])]
-        reg_value += [hex(self._regMap['dllClockDelay_CH1'] << 4 | self._regMap['dllClockDelay_CH0'])]
-        reg_value += [hex(self._regMap['Rx_Enable'] << 6 | self._regMap['Rx_setCM'] << 5 | self._regMap['Rx_enTermination'] << 4 | self._regMap['Rx_invData'] << 3 | self._regMap['Rx_Equa'])]
-        reg_value += [hex(self._regMap['Tx1_DL_ATT'] << 4 | self._regMap['Tx1_Dis_DL_Emp'] << 3 | self._regMap['Tx1_DL_SR'])]
-        reg_value += [hex(self._regMap['Tx1_Dis_DL_BIAS'] << 1 | self._regMap['Tx1_Dis_DL_LPF_BIAS'])]
-        reg_value += [hex(self._regMap['Tx2_DL_ATT'] << 4 | self._regMap['Tx2_Dis_DL_Emp'] << 3 | self._regMap['Tx2_DL_SR'])]
-        reg_value += [hex(self._regMap['Tx2_Dis_DL_BIAS'] << 1 | self._regMap['Tx2_Dis_DL_LPF_BIAS'])]
+        reg_value += [self._regMap['CH1_Dis_EQ_LF'] << 5 | self._regMap['CH1_EQ_ATT'] << 3 | self._regMap['CH1_CML_AmplSel']]
+        reg_value += [self._regMap['CH1_CTLE_HFSR'] << 4 | self._regMap['CH1_CTLE_MFSR']]
+        reg_value += [self._regMap['CH1_Disable'] << 2 | self._regMap['CH1_Dis_DFF'] << 1 | self._regMap['CH1_Dis_LPF']]
+        reg_value += [self._regMap['CH2_Dis_EQ_LF'] << 5 | self._regMap['CH2_EQ_ATT'] << 3 | self._regMap['CH2_CML_AmplSel']]
+        reg_value += [self._regMap['CH2_CTLE_HFSR'] << 4 | self._regMap['CH2_CTLE_MFSR']]
+        reg_value += [self._regMap['CH2_Disable'] << 2 | self._regMap['CH2_Dis_DFF'] << 1 | self._regMap['CH2_Dis_LPF']]
+        reg_value += [self._regMap['CH3_Dis_EQ_LF'] << 5 | self._regMap['CH3_EQ_ATT'] << 3 | self._regMap['CH3_CML_AmplSel']]
+        reg_value += [self._regMap['CH3_CTLE_HFSR'] << 4 | self._regMap['CH3_CTLE_MFSR']]
+        reg_value += [self._regMap['CH3_Disable'] << 2 | self._regMap['CH3_Dis_DFF'] << 1 | self._regMap['CH3_Dis_LPF']]
+        reg_value += [self._regMap['CH4_Dis_EQ_LF'] << 5 | self._regMap['CH4_EQ_ATT'] << 3 | self._regMap['CH4_CML_AmplSel']]
+        reg_value += [self._regMap['CH4_CTLE_HFSR'] << 4 | self._regMap['CH4_CTLE_MFSR']]
+        reg_value += [self._regMap['CH4_Disable'] << 2 | self._regMap['CH4_Dis_DFF'] << 1 | self._regMap['CH4_Dis_LPF']]
+        reg_value += [self._regMap['CH5_Dis_EQ_LF'] << 5 | self._regMap['CH5_EQ_ATT'] << 3 | self._regMap['CH5_CML_AmplSel']]
+        reg_value += [self._regMap['CH5_CTLE_HFSR'] << 4 | self._regMap['CH5_CTLE_MFSR']]
+        reg_value += [self._regMap['CH5_Disable'] << 2 | self._regMap['CH5_Dis_DFF'] << 1 | self._regMap['CH5_Dis_LPF']]
+        reg_value += [self._regMap['CH6_Dis_EQ_LF'] << 5 | self._regMap['CH6_EQ_ATT'] << 3 | self._regMap['CH6_CML_AmplSel']]
+        reg_value += [self._regMap['CH6_CTLE_HFSR'] << 4 | self._regMap['CH6_CTLE_MFSR']]
+        reg_value += [self._regMap['CH6_Disable'] << 2 | self._regMap['CH6_Dis_DFF'] << 1 | self._regMap['CH6_Dis_LPF']]
+        reg_value += [self._regMap['CH7_Dis_EQ_LF'] << 5 | self._regMap['CH7_EQ_ATT'] << 3 | self._regMap['CH7_CML_AmplSel']]
+        reg_value += [self._regMap['CH7_CTLE_HFSR'] << 4 | self._regMap['CH7_CTLE_MFSR']]
+        reg_value += [self._regMap['CH7_Disable'] << 2 | self._regMap['CH7_Dis_DFF'] << 1 | self._regMap['CH7_Dis_LPF']]
+        reg_value += [self._regMap['dllEnable'] << 1 | self._regMap['dllCapReset']]
+        reg_value += [self._regMap['dllForceDown'] << 4 | self._regMap['dllChargePumpCurrent']]
+        reg_value += [self._regMap['dllClockDelay_CH7'] << 4 | self._regMap['dllClockDelay_CH6']]
+        reg_value += [self._regMap['dllClockDelay_CH5'] << 4 | self._regMap['dllClockDelay_CH4']]
+        reg_value += [self._regMap['dllClockDelay_CH3'] << 4 | self._regMap['dllClockDelay_CH2']]
+        reg_value += [self._regMap['dllClockDelay_CH1'] << 4 | self._regMap['dllClockDelay_CH0']]
+        reg_value += [self._regMap['Rx_Enable'] << 6 | self._regMap['Rx_setCM'] << 5 | self._regMap['Rx_enTermination'] << 4 | self._regMap['Rx_invData'] << 3 | self._regMap['Rx_Equa'] << 1 | self._regMap['Dis_Tx']]
+        reg_value += [self._regMap['Tx1_DL_ATT'] << 4 | self._regMap['Tx1_Dis_DL_Emp'] << 3 | self._regMap['Tx1_DL_SR']]
+        reg_value += [self._regMap['Tx1_Dis_DL_BIAS'] << 1 | self._regMap['Tx1_Dis_DL_LPF_BIAS']]
+        reg_value += [self._regMap['Tx2_DL_ATT'] << 4 | self._regMap['Tx2_Dis_DL_Emp'] << 3 | self._regMap['Tx2_DL_SR']]
+        reg_value += [self._regMap['Tx2_Dis_DL_BIAS'] << 1 | self._regMap['Tx2_Dis_DL_LPF_BIAS']]
         return reg_value
